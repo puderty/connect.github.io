7b30ae72da59b7a5a750be445c3b3d27ffd0fb22
weilong.yang <weilong.yang@okg.com>
2024-12-16 16:49:16 +0800

    test

--- testy/assets/View-B80gwQ5q.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./View-DWorWXen.js";import{d as t,_ as c,c as s,r as a,o as r}from"./index-B2eaHZpE.js";import"./ConnectLayout-BPWVh1h8.js";const i=t({components:{BtcBaseView:o},setup(){return{chainId:"btc:mainnet"}}});function p(e,m,d,_,B,f){const n=a("BtcBaseView");return r(),s(n,{chainId:e.chainId},null,8,["chainId"])}const w=c(i,[["render",p]]);export{w as default};
