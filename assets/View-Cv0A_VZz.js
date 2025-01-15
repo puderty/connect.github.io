b4f40e7bdcb8d4b3946d080a2f5dfc7286ca7f80
gonghua.zhang <gonghua.zhang@okg.com>
2025-01-15 18:42:44 +0800

    update

---
+++ assets/View-Cv0A_VZz.js
@@ -0,0 +1,1 @@
+import{B as o}from"./View-G60NOFce.js";import{d as t,_ as c,c as s,r as a,o as r}from"./index-CVQZtFQN.js";import"./ConnectLayout-DCXim5oJ.js";const i=t({components:{BtcBaseView:o},setup(){return{chainId:"btc:mainnet"}}});function p(e,m,d,_,B,f){const n=a("BtcBaseView");return r(),s(n,{chainId:e.chainId},null,8,["chainId"])}const w=c(i,[["render",p]]);export{w as default};
--- assets/View-DYDa0oaO.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./View-CGa3WeuO.js";import{d as t,_ as c,c as s,r as a,o as r}from"./index-t5ByIVUM.js";import"./ConnectLayout-C2tMyHmU.js";const i=t({components:{BtcBaseView:o},setup(){return{chainId:"btc:mainnet"}}});function p(e,m,d,_,B,f){const n=a("BtcBaseView");return r(),s(n,{chainId:e.chainId},null,8,["chainId"])}const w=c(i,[["render",p]]);export{w as default};
